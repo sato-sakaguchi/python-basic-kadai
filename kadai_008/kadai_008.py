f18da22856e910010235af576fcdb0799e89da50
sato-sakaguchi <135315904+sato-sakaguchi@users.noreply.github.com>
2023-06-06 18:03:38 +0900

    Update kadai_008.py

--- kadai_008/kadai_008.py
+++ kadai_008/kadai_008.py
@@ -1,8 +1,4 @@
-import random
-var = random.randint(1,101)
-# var = int(var)
-print(var)
-#変数varの値を出力：確認用
+var = int(var)
 
 if var % 3 == 0 and var % 5 ==0:
     print("FizzBuzz")
@@ -15,16 +11,3 @@ elif var % 3 == 0:
 
 else:
     print(var)
-
-# for var in range(1,101):
-#     if var % 3 == 0 and var % 5 ==0:
-#         print("FizzBuzz")
-
-#     elif var % 5 == 0:
-#         print("Buzz")
-
-#     elif var % 3 == 0:
-#         print("Fizz")
-
-#     else:
-#         print(var)
