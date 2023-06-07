c45c6c4b6cf0aa2339b038b7f1684bada236efef
sato-sakaguchi <135315904+sato-sakaguchi@users.noreply.github.com>
2023-06-07 11:13:33 +0900

    kadai_011ディレクトリにkadai_011.pyをアップロード

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,11 @@
+list = ["水","金","地","火","木","土","天","海","冥"]
+
+#for文を利用したリストの出力コード
+for i in list:
+    print(i)
+
+#while文を利用したリストの出力コード
+j = 0
+while j < len(list):
+    print(list[j])
+    j += 1
